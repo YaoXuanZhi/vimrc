df35d9005ee5b22ba51f2a0230527b2fc899938f
yaoxuanzhi <yaoxuanzhi@outlook.com>
2019-08-11 15:10:16 +0800

    更新

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+cur_time=`date "+%Y-%m-%d_%H-%M-%S"`
+mv /usr/share/vim/vimfiles /usr/share/vim/vimfiles_$cur_time
+mv ~/.vimrc ~/.vimrc_$cur_time
+
 git submodule update --init --recursive
 cp .vimrc ~/.vimrc
 cp -r vimfiles /usr/share/vim/vimfiles
